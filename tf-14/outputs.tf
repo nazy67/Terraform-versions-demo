5d4c0a122af559fb7f513b58fecf5e415edc2923
nazy67 <nazykh67@gmail.com>
2021-08-27 13:12:48 -0500

    modified outputs

--- tf-14/outputs.tf
+++ tf-14/outputs.tf
@@ -1,7 +1,7 @@
 output "rds_db_name" {
-  value = aws_db_instance.rds_db.password
+  value = aws_db_instance.rds-db.password
 }
 
 output "rds_db_username" {
-  value = aws_db_instance.rds_db.username
+  value = aws_db_instance.rds-db.username
 }
